c2a1739338c5bba983f831c4e085a349c5a9fbe1
Pranav Dani <pranavpdani@gmail.com>
2021-08-21 21:04:35 +0530

    feat: add carousel update: quote background
    Co-authored-by: Darshan Rander <darshandrander@gmail.com>

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,102 +1,98 @@
-import Slider from "react-animated-slider";
+// import Slider from "react-animated-slider";
 import "react-animated-slider/build/horizontal.css";
 import "./Home.css";
 import Card from "./components/card";
+import React from "react";
+import Carousel from "react-material-ui-carousel";
+import { Paper } from "@material-ui/core";
 
-export interface HomeProps {
-
+interface CarouselItemProps {
+  title: string;
+  image: string;
 }
 
-const content = [
-    {
-        title: "Vulputate Mollis Ultricies Fermentum Parturient",
-        description:
-            "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.",
-        button: "Read More",
-        image: "https://i.imgur.com/ZXBtVw7.jpg",
-    },
-    {
-        title: "Tortor Dapibus Commodo Aenean Quam",
-        description:
-            "Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.",
-        button: "Discover",
-        image: "https://i.imgur.com/DCdBXcq.jpg",
-    },
-    {
-        title: "Phasellus volutpat metus",
-        description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.",
-        button: "Buy now",
-        image: "https://i.imgur.com/DvmN8Hx.jpg",
-    }
+const content: Array<CarouselItemProps> = [
+  {
+    title: "More than 10 years of experience.",
+    image: "Image_02.jpg",
+  },
+  {
+    title: "Simplicity is the ultimate sophistication.",
+    image: "Calacatta_01.jpg",
+  },
+  {
+    title: "Every stone has a story.",
+    image: "Image_03.jpg",
+  },
 ];
 
-const Home: React.FunctionComponent<HomeProps> = () => {
+function Home() {
+  function Item(props: CarouselItemProps) {
     return (
-        <div>
-            <Slider>
-                {content.map((item, index) => (
-                    <div
-                        key={index}
-                        className="slider-content"
-                        style={{ background: `url('${item.image}') no-repeat center center` }}
-                    >
-                        {/* <div className="inner">
-                            <h1>{item.title}</h1>
-                            <p>{item.description}</p>
-                            <button>{item.button}</button>
-                        </div> */}
-                    </div>
-                ))}
-            </Slider>
-            <div className="quote">
-                Marble is forever.
-            </div>
-
-            <h1>CATEGORIES</h1>
-            <section className="categories">
-                <div className="internal">
-                    <Card title="INDIAN MARBLE" img="indian-marble.jpg" />
-                    <Card title="IMPORTED MARBLE" img="imported-marble.jpg" />
-                    <Card title="INDIAN GRANITE" img="indian-granite.jpg" />
-                </div>
-                <div className="internal">
-                    <Card title="IMPORTED GRANITE" img="imported-granite.jpg" />
-                    <Card title="STONE" img="stone.jpg" />
-                    <Card title="COMPOSITE" img="composite.jpg" />
-                </div>
-            </section>
-
-
-            <h1>CONTACT US</h1>
-            <section className="contact">
-                <form action="">
-                    <label htmlFor="">Name</label>
-                    <input type="text" placeholder="Your Name" />
-                    <div className="group">
-                        <div className="user-input">
-                            <label htmlFor="">Phone number</label>
-                            <input type="text" placeholder="Your Phone no" />
-                        </div>
-                        <div style={{ width: "10px" }}></div>
-                        <div className="user-input">
-                            <label htmlFor="">Email</label>
-                            <input type="text" placeholder="Your Email" />
-                        </div>
-                    </div>
-
-                    <label htmlFor="">Message</label>
-                    <textarea rows={5} name="" id="" cols={30} placeholder="Write your requirement"></textarea>
-
-                    <button>
-                        Submit
-                    </button>
-
-                </form>
-            </section>
-        </div >
+      <Paper className="carousel-item">
+        <img
+          src={`${process.env.PUBLIC_URL}/assets/carousel/${props.image}`}
+          alt="img"
+        />
+        <h2>{props.title}</h2>
+        <button>Check it out!</button>
+      </Paper>
     );
+  }
+  return (
+    <div>
+      <Carousel className="carousel">
+        {content.map((item, index) => (
+          <Item key={index} title={item.title} image={item.image} />
+        ))}
+      </Carousel>
+      <div className="quote">Marble is forever.</div>
+
+      <h1>CATEGORIES</h1>
+      <section className="categories">
+        <div className="internal">
+          <Card title="INDIAN MARBLE" img="indian-marble.jpg" />
+          <Card title="IMPORTED MARBLE" img="imported-marble.jpg" />
+          <Card title="INDIAN GRANITE" img="indian-granite.jpg" />
+        </div>
+        <div className="internal">
+          <Card title="IMPORTED GRANITE" img="imported-granite.jpg" />
+          <Card title="STONE" img="stone.jpg" />
+          <Card title="COMPOSITE" img="composite.jpg" />
+        </div>
+      </section>
+
+      <h1>CONTACT US</h1>
+      <section className="contact">
+        <form action="">
+          <label htmlFor="">Name</label>
+          <input type="text" placeholder="Your Name" />
+          <div className="group">
+            <div className="user-input">
+              <label htmlFor="">Phone number</label>
+              <input type="text" placeholder="Your Phone no" />
+            </div>
+            <div style={{ width: "10px" }}></div>
+            <div className="user-input">
+              <label htmlFor="">Email</label>
+              <input type="text" placeholder="Your Email" />
+            </div>
+          </div>
+
+          <label htmlFor="">Message</label>
+          <textarea
+            rows={5}
+            name=""
+            id=""
+            cols={30}
+            placeholder="Write your requirement"
+          ></textarea>
+
+          <button>Submit</button>
+        </form>
+      </section>
+    </div>
+  );
 }
 
 export default Home;
-
