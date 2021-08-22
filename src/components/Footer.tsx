2128fe1742e344a9d171d47a1f35c3b6cc29a25c
Pranav Dani <pranavpdani@gmail.com>
2021-08-22 13:02:40 +0530

    update: Improved Footer
    Co-authored-by: Darshan Rander <darshandrander@gmail.com>
    Co-authored-by: Dhairya Panjwani <dhairyapanjwani@users.noreply.github.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,43 @@
-import "./Footer.css"
+import "./Footer.css";
 
 function Footer() {
-    return (
-        <footer className="footer">
-            <h5>
-                Dr. Rajendra Prasad Nagar,
-                Navpada, Vile Parle East,
-                Mumbai, Maharashtra 400047
-            </h5>
-            <div className="copyrights">
-                <p> Copyright reserved 2021</p>
-                <p> Copyright reserved 2021</p>
-            </div>
-        </footer>
-    )
+  return (
+    <footer className="footer">
+      <h5>
+        Dr. Rajendra Prasad Nagar, Navpada, Vile Parle East, Mumbai, Maharashtra
+        400047
+      </h5>
+      <div className="copyrights">
+        <p> © Copyright reserved 2021</p>
+        <p>
+          Designed and Developed by{" "}
+          <a
+            href="https://linkedin.com/in/darshan-rander"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Darshan
+          </a>
+          ,{" "}
+          <a
+            href="https://linkedin.com/in/pranav-dani/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Pranav
+          </a>
+          ,{" "}
+          <a
+            href="https://linkedin.com/in/dhairya-panjwani"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Dhairya
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
